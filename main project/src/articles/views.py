776490594da2dce0930bac1f6150316d7ef5e32c
unknown <ziyadtr101@gmail.com>
2021-04-04 20:01:36 +0200

    added search article functionality

--- main project/src/articles/views.py
+++ main project/src/articles/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .mdoels import Article
 
-# Create your views here.
+
+def search(request, query):
+    if request.method == "GET":
+        query = request.GET.get('query', None)
+        if query:
+            results = Article.objects.filter(title__icontains=query | content__icontains=query)
+            context = {'results':results}
+            return render(request, 'article-search.html', context)
